f8f0ead0ce244ec4adc17efd6e083aee2582c640
John Olorunsogo <olorunsogojohn6@gmail.com>
2023-06-30 10:03:25 +0000

    weatherme web  template design

---
+++ lib/desktop_layout.dart
@@ -0,0 +1,272 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DesktopLayout extends StatelessWidget {
+  DesktopLayout({super.key});
+  final TempUnit tempUnit = TempUnit.celsius;
+  final PageController pageController = PageController(
+    initialPage: 1,
+    viewportFraction: 0.65,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.sizeOf(context);
+
+    return Container(
+      height: size.height,
+      width: size.width,
+      decoration: const BoxDecoration(
+        color: Color(0xff252323),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'WeatherMe',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: size.width * 0.051,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        '21:00pm',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: size.width * 0.014,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Today',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: size.width * 0.022,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: size.width * 0.05),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Tomorrow',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: size.width * 0.022,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: size.width * 0.05),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Monthly Forecast',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: size.width * 0.022,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: size.width * 0.05),
+                    ],
+                  )
+                ],
+              ),
+
+              //unit switch
+              Row(
+                children: [
+                  TempUnitSwitch(),
+                ],
+              ),
+
+//search bar
+
+              Container(
+                width: size.width * 0.52,
+                height: size.width * 0.048,
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFD9D9D9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(size.width * 0.0183),
+                  ),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: TextField(
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: size.width * 0.018,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Image.asset(
+                            'assets/🦆 icon _search_.png',
+                            // height: 5,
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: size.width * 0.02,
+                          vertical: size.width * 0.0183,
+                        ),
+                        hintText: 'Search location...',
+                        hintStyle: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: size.width * 0.022,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: size.width,
+                height: size.width * 0.3257,
+                child: PageView(
+                  controller: pageController,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    WeatherCard(size: size),
+                    // SizedBox(width: size.width * 0.001),
+                    WeatherCard(size: size),
+                    WeatherCard(size: size),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TempUnitSwitch extends StatefulWidget {
+  TempUnitSwitch({this.tempUnit = TempUnit.celsius, super.key});
+  TempUnit tempUnit;
+
+  @override
+  State<TempUnitSwitch> createState() => _TempUnitSwitchState();
+}
+
+class _TempUnitSwitchState extends State<TempUnitSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedToggleSwitch<TempUnit>.dual(
+      current: widget.tempUnit,
+      first: TempUnit.fahrenheit,
+      second: TempUnit.celsius,
+      dif: 1.0,
+      borderColor: Colors.transparent,
+      borderWidth: 5.0,
+      height: 30,
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black26,
+          spreadRadius: 1,
+          blurRadius: 2,
+          offset: Offset(0, 1.5),
+        ),
+      ],
+      onChanged: (b) {
+        setState(() => widget.tempUnit = b);
+        // return Future.delayed(const Duration(seconds: 1));
+      },
+      colorBuilder: (b) => b == TempUnit.celsius ? Colors.blue : Colors.green,
+      iconBuilder: (value) => value == TempUnit.celsius
+          ? SvgPicture.asset(
+              'assets/°C.svg',
+              height: 10,
+              width: 18,
+            )
+          : SvgPicture.asset(
+              'assets/°F.svg',
+              height: 10,
+              width: 18,
+            ),
+      textBuilder: (value) => value == TempUnit.celsius
+          ? const Center(
+              child: Text(
+                '°F',
+                style: TextStyle(color: Colors.black, fontSize: 12),
+              ),
+            )
+          : const Center(child: Text('°C')),
+    );
+  }
+}
+
+class WeatherCard extends StatelessWidget {
+  const WeatherCard({required this.size, super.key});
+  final Size size;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: size.width * 0.03),
+        // width: 817,
+        // height: 425,
+        decoration: ShapeDecoration(
+          gradient: const LinearGradient(
+            begin: Alignment(-0.9, 0),
+            end: Alignment(0.5, 0.8),
+            colors: [Color(0xFFAD36CB), Color(0xFF323233)],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(size.width * 0.02343),
+          ),
+          shadows: [
+            BoxShadow(
+              color: const Color(0x3F000000),
+              blurRadius: size.width * 0.003,
+              offset: Offset(0, size.width * 0.003),
+              spreadRadius: 0,
+            ),
+            BoxShadow(
+              color: const Color(0x3FFFF9F9),
+              blurRadius: size.width * 0.003,
+              offset: Offset(0, size.width * 0.003),
+              spreadRadius: 0,
+            ),
+            BoxShadow(
+              color: const Color(0x3FFCF5F5),
+              blurRadius: size.width * 0.003,
+              offset: Offset(0, size.width * 0.003),
+              spreadRadius: 0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+enum TempUnit { celsius, fahrenheit }
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:weatherme_website/desktop_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return const Text('Screen too small');
+        } else {
+          return DesktopLayout();
+        }
+      },
+    ));
+  }
+}
